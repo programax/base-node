6ba2983967adcbb8b43564e93bdba1f932d6c5ca
leonso <leonziyo@gmail.com>
2021-06-09 20:29:10 -0500

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+  env: {
+    es6: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.js'],
+      },
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './jsconfig.json',
+  },
+  plugins: ['prettier', 'import'],
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+  },
 };
